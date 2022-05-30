cc4f6b53d4e2f27fcf49605181e1d20ab57c2567
IPablo271 <69815580+IPablo271@users.noreply.github.com>
2022-05-30 10:29:06 -0600

    First Changes
    First Changes to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { isMotionValue } from 'framer-motion';
 import React from 'react';
 
 // import components
+import Header from './Components/Header';
 
 
 const App = () => {
   return (
-    <div>react app</div>
+    <div>
+      <Header/>
+      <div style={{height:'2000px'}}></div>
+    </div>
   );
 };
 
